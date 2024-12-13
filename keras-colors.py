4ddd5d679d14f1c65d3b6dc31f705c8964236985
unknown <shashwotpradhan@gmail.com>
2024-12-13 13:19:00 +0545

    import

--- keras-colors.py
+++ keras-colors.py
@@ -2,3 +2,4 @@ import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.keras import preprocessing
 from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.models import Sequential
