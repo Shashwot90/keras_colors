19ea8dc8b0f1430e7c72d33a6411a01a9a81c747
unknown <shashwotpradhan@gmail.com>
2024-12-15 14:18:56 +0545

    import

--- keras-colors.py
+++ keras-colors.py
@@ -4,3 +4,4 @@ from tensorflow.python.keras import preprocessing
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM, Reshape
+import numpy as np
